d5fd7b27a9ec9f53ef36a1cfec0b3115fed8b244
wushaohe <wushaohe@hanfeng.cn>
2019-07-10 15:50:46 +0800

    添加api文档

--- server/src/main/java/vip/wush/server/jenkins/controller/HelloController.java
+++ server/src/main/java/vip/wush/server/jenkins/controller/HelloController.java
@@ -13,6 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    /**
+     * @api {GET} /hello  demo-hello 测试接口
+     * @apiGroup spring-jenkins-demo
+     * @apiName HelloController
+     * @apiDescription  测试接口
+     *
+     * @apiParam {String} name 名字
+     * @apiParamExample 请求示例
+     * GET: /hello?name=test
+     *
+     * @apiSuccess {number} errcode      返回码
+     * @apiSuccess {String} errmsg       返回码描述
+     * @apiSuccessExample 正确时的返回JSON数据包如下
+     * {
+     *    "errcode":0,
+     *    "errmsg":"ok"
+     * }
+     *
+     * @apiError   -1     系统繁忙，请稍候再试
+     *
+     * @apiErrorExample 错误时的返回JSON数据包如下（示例为系统异常）
+     * {
+     *     "errcode": -1,
+     *     "errmsg": "系统繁忙，请稍候再试"
+     * }
+     */
     @GetMapping("/hello")
     public String hello(String name){
         if(name == null || "".equals(name)){
@@ -20,6 +46,5 @@ public class HelloController {
         }
         return name;
     }
-
 }
 
